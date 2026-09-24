86e971771c98b3655abc8bea8227254e8712d597
Shreyash <shreyashnreddy2007@gmail.com>
2026-09-24 21:51:24 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://real-time-multi-building-routing-for-t71s.onrender.com/api',
 });
 
 export const getNetwork = async () => (await api.get('/network')).data;
